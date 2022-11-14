7496d18e5ebb5205f57397dbb52f0bed389962f6
Iryna Dachuk <iradachuk@gmail.com>
2022-11-14 23:13:39 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,57 +2,54 @@ USERS = {}
 
 
 def input_error(func):
-    def inner(data):
+    def inner(*args, **kwargs):
         try:
-            result = func(data)
-            return result
+            return func(*args, **kwargs)
         except KeyError:
-            print('No user with given name.')
+            return 'No user with given name.'
         except ValueError:
-            print('Give me name and phone, please.')
+            return 'Give me name and phone, please.'
         except IndexError:
-            print('Give me name and phone, please.')
+            return 'Give me name and phone, please.'
     return inner
 
 
 def greeting(_):
-    print('How can I help you?')
+    return 'How can I help you?'
 
 
 def good_bye(_):
-    print('Good bye!')
+    return 'Good bye!'
 
 
 @input_error
 def add_contact(data):
-    name = data[0]
-    phone = data[1]
+    name, phone, *_ = data
     USERS[name] = phone
-    print(f'The user with name {name} and phone {phone} was added!')
+    return f'The user with name {name} and phone {phone} was added!'
 
 
 @input_error
 def change_phone(data):
-    name = data[0]
-    phone = data[1]
+    name, phone, *_ = data
     for key in USERS.keys():
         if key == name:
             USERS[name] = phone
-    print(f'The phone number for name {name} was changed!')
+    return f'The phone number for name {name} was changed!'
 
 
 @input_error
 def show_phone(data):
-    print(f'The phone number is: {USERS[data[0]]}')
+    return f'The phone number is: {USERS[data[0]]}'
 
 
 def show_all(_):
     for name, phone in USERS.items():
-        print(f'{name} - {phone}')
+        return f'{name} - {phone}'
 
 
 def unknown_command(_):
-    print('This command is unknown!')
+    return 'This command is unknown!'
 
 
 def get_handler(user_input):
@@ -82,11 +79,12 @@ def main():
     flag = True
     while flag:
         user_input = input('>>> ')
-        if user_input in ('good_bye', 'exit', 'close'):
+        if user_input in ('good_bye', 'exit', 'close', '.'):
             flag = False
         handler, data = get_handler(user_input)
         try:
-            handler(data)
+            result = handler(data)
+            print(result)
         except KeyError:
             print('Unknown command')
 
